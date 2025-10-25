f1b03fa26de4407189ef5d51b47a908b16012d71
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-10-25 12:28:41 +0500

    To many changes and added a few file

---
+++ JS_Day01/consolelog/index.js
@@ -0,0 +1,18 @@
+// Basic Log how used it
+// 1) print a string, number and a Boolean
+console.log("hello World"); // A string
+console.log(42); // A number
+console.log(true); // A boolean
+
+// Log Multiple values
+// 2) log the following
+console.log(23 * 4);
+console.log(12 + 2);
+console.log(10 - 4);
+console.log(42 / 2);
+console.log(12 == 12);
+console.log(12 != 40); //this condition is true that the small value is not equal to the large value
+console.log(12 === 40); //they will checked the both values data types are same or not if not it will be false.
+console.log(31 >= 31); //the condition is true, because the values are same
+console.log(31 != 31); //the condition is false, because the  values are same
+console.log(31 < 31); //the condition is false , because the values are same
